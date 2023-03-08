1d39a72b485ee3983cb78b5b8424cc0daa6a8864
codelzn <lzn724958732@gmail.com>
2023-03-08 19:56:03 +0900

    update:add-gsap

--- src/Experience/world/Controls.ts
+++ src/Experience/world/Controls.ts
@@ -1,25 +1,38 @@
 import * as THREE from "three"
 import gsap from "gsap"
 import Experience from ".."
+import Sizes from "../utils/Sizes"
 import Room from "./Room"
 import { ScrollTrigger } from "gsap/ScrollTrigger"
 export default class Controls {
 	private scene: THREE.Scene
-  private orthographicCamera?: THREE.OrthographicCamera
-  private timeline?: gsap.core.Timeline
-  private room: THREE.Object3D
+	private orthographicCamera?: THREE.OrthographicCamera
+	private sizes: Sizes
+	private timeline?: gsap.core.Timeline
+	private room: THREE.Object3D
 	constructor(private readonly experience: Experience) {
-    this.scene = this.experience.scene
-    this.room = this.experience.world.room!.actualRoom
-    this.orthographicCamera = this.experience.camera.orthographicCamera
-    gsap.registerPlugin(ScrollTrigger)
-    document.querySelector<HTMLDivElement>(".page")!.style.overflow = "visible";
-    this.setPath()
+		this.scene = this.experience.scene
+		this.sizes = this.experience.sizes
+		this.room = this.experience.world.room!.actualRoom
+		this.orthographicCamera = this.experience.camera.orthographicCamera
+		gsap.registerPlugin(ScrollTrigger)
+		document.querySelector<HTMLDivElement>(".page")!.style.overflow = "visible"
+		this.setPath()
 	}
-  private setPath() {
-    this.timeline = gsap.timeline()
-  }
+	private setPath() {
+		this.timeline = gsap.timeline()
+		this.timeline.to(this.room.position, {
+			x: () => this.sizes.width * 0.00094,
+			scrollTrigger: {
+				trigger: ".first-move",
+				markers: true,
+				start: "top top",
+				end: "bottom bottom",
+        scrub: 0.6,
+        invalidateOnRefresh: true,
+			},
+		})
+	}
 	public resize() {}
-	public update() {
-	}
+	public update() {}
 }
